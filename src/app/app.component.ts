ee481a96b40c7e6cc37974d74766f50c610e0eaf
mattdsteele <orphum@gmail.com>
2018-01-17 11:28:50 -0600

    Demonstrate error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { tileLayer, latLng, Layer, marker, icon, MarkerOptions } from 'leaflet';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: '...'
+      })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
+  tooltipClickedTimes = 0;
+  layers: Layer[] = [];
+  ngOnInit() {
+    console.log('initting');
+    const markerOptions: MarkerOptions = {
+      icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      })
+    };
+    const mark = marker(latLng(46.879966, -121.726909), markerOptions);
+    mark.addEventListener('click', evt => {
+      console.log('clicked tooltip');
+      this.tooltipClickedTimes++;
+    });
+    this.layers.push(mark);
+  }
+  forceUpdate() {
+    this.tooltipClickedTimes++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [LeafletModule.forRoot(), BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
